45c5159b0435e698ba8befc660a76b931efe38df
zentol <s.motsu@web.de>
2013-11-14 22:10:15 +0100

    major changes
    preprocessing added
    - <=>/<~> substituted
    -quantors moved to the front

--- Codec/TPTP.hs
+++ Codec/TPTP.hs
@@ -21,7 +21,7 @@ t_b = parse "fof(test,axiom,(~a&b))."
 t_negate =  parse "fof(test,axiom,a)."
 t_negate2 = parse "fof(test,axiom,~a)."
 t_eq = parse "fof(test,axiom,a<=>b)."
-t_quant = parse "fof(test,axiom,? [A,Y] :(a|(a|b)))."
+t_quant = parse "fof(test,axiom,? [A,Y]:fix(A))."
 t_pred = parse "fof(test,axiom,fix(A,B))."
 t_doublenegation = parse "fof(test,axiom,~(~(a)))."
 t_scf1 = parse "fof(test,axiom,a&(b|c)&c)."
@@ -86,6 +86,136 @@ walk_f (F (Identity (BinOp op1 (:|:)   op2))) 	= (F (Identity (BinOp (walk_f(op1
 
 
 
+--preProcessing
+
+preprocess f = quantor_main(equality_main(get_formula(get_list_element(f))))
+
+
+
+
+
+
+
+
+
+--replace equalities
+
+equality_main f = find_eq_f f
+
+find_eq_t [] = []
+find_eq_t ((T (Identity ( Var v))):[]) = ((T (Identity( Var v))):[])
+find_eq_t ((T (Identity ( Var v))):xs) = ((T (Identity( Var v))):find_eq_t(xs))
+
+find_eq_f (F (Identity (Quant All cont op1)))		= (F (Identity (Quant All cont (find_eq_f(op1)))))
+find_eq_f (F (Identity (Quant Exists cont op1)))	= (F (Identity (Quant Exists cont (find_eq_f(op1)))))
+find_eq_f (F (Identity (PredApp aw t)))				= (F (Identity (PredApp aw (walk_t(t)))))
+find_eq_f (F (Identity ((:~:) op1)))				= (F (Identity ((:~:) (find_eq_f(op1)))))
+
+find_eq_f (F (Identity (BinOp op1 (:<=>:) op2))) 	= find_eq_f(F (Identity (BinOp ((F (Identity (BinOp op1 (:=>:)  op2)))) (:&:) ((F (Identity (BinOp op1 (:<=:)  op2)))))))
+
+find_eq_f (F (Identity (BinOp op1 (:<=:)  op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:<=:)  (find_eq_f(op2)))))
+find_eq_f (F (Identity (BinOp op1 (:=>:)  op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:=>:)  (find_eq_f(op2)))))
+find_eq_f (F (Identity (BinOp op1 (:<~>:) op2))) 	= find_eq_f(F (Identity ((:~:) (F (Identity (BinOp ((F (Identity (BinOp op1 (:=>:)  op2)))) (:&:) ((F (Identity (BinOp op1 (:<=:)  op2))))))))))
+find_eq_f (F (Identity (BinOp op1 (:~|:)  op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:~|:)  (find_eq_f(op2)))))
+find_eq_f (F (Identity (BinOp op1 (:~&:)  op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:~&:)  (find_eq_f(op2)))))
+find_eq_f (F (Identity (BinOp op1 (:&:)   op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:&:)   (find_eq_f(op2)))))
+find_eq_f (F (Identity (BinOp op1 (:|:)   op2))) 	= (F (Identity (BinOp (find_eq_f(op1)) (:|:)   (find_eq_f(op2)))))
+
+
+
+
+
+
+
+
+--move quantor to the front
+
+quantor_main f = add_quantor(remove_quantor(r_v(f)),find_quantor(r_v(f),[],[]))
+
+--find all quantors
+
+find_quantor ((F (Identity (Quant All cont op1)))		,x,y)= find_quantor (op1,x++cont,y)
+find_quantor ((F (Identity (Quant Exists cont op1)))	,x,y)= find_quantor (op1,x,y++cont)
+find_quantor ((F (Identity ((:~:) op1)))				,x,y)= find_quantor (op1,x,y)
+find_quantor ((F (Identity (BinOp op1 op op2))) 		,x,y)= find_quantor_merge(find_quantor(op1,[],[]),find_quantor(op2,[],[]),(x,y))
+find_quantor (_,x,y)=(x,y)
+find_quantor_merge ((x1,y1),(x2,y2),(x3,y3))=(x1++x2++x3,y1++y2++y3)
+
+--rename variables
+
+r_v (f) = r_v_s (f,[],['X'])
+
+--search
+r_v_s ((F (Identity ((:~:) op1))),vars,path) 		= (F (Identity ((:~:) (r_v_s(op1,vars,path)))))
+r_v_s ((F (Identity (BinOp op1 op op2))),vars,path) = (F (Identity (BinOp (r_v_s(op1,vars,path++['L'])) op (r_v_s(op2,vars,path++['R'])))))
+r_v_s ((F (Identity (Quant t c op1))),vars,path)	= (F (Identity (Quant t (r_v_r_q(c,path)) (r_v_f(op1,(r_v_u(c,vars,(r_v_r_q(c,path)))),path)))))
+r_v_s (f,vars,path) = f
+
+--found
+r_v_f ((F (Identity (PredApp aw t))),vars,path)			= (F (Identity (PredApp aw (r_v_r(t,vars)))))
+r_v_f ((F (Identity ((:~:) op1))),vars,path) 		 	= (F (Identity ((:~:) (r_v_f(op1,vars,path)))))
+r_v_f ((F (Identity (BinOp op1 op op2))),vars,path) 	= (F (Identity (BinOp (r_v_f(op1,vars,path++['L'])) op (r_v_f(op2,vars,path++['R'])))))
+r_v_f ((F (Identity (Quant t (c:cs) op1))),vars,path)	= (F (Identity (Quant t (r_v_r_q((c:cs),path)) (r_v_f(op1,(r_v_j(c:cs,vars,(r_v_r_q((c:cs),path)))),path)))))
+
+--rename quantor
+r_v_r_q (c,p) = r_v_r_q_r (c,p,(length(c))-1)
+
+r_v_r_q_r ([],p,q)=[]
+r_v_r_q_r ((V c):cs,p,q) = (V (p++(show(q)))):(r_v_r_q_r(cs,p,q-1))
+
+--unite
+r_v_u ([],v,_) = v
+r_v_u ((V c):cs,v,(V p):ps) = r_v_u(cs,v++[((V c),p)],ps)
+
+--join
+r_v_j ([],v,_) = v
+r_v_j ((V c):cs,v,(V p):ps) = r_v_j(cs,v++r_v_c((V c),v,p),ps)
+
+--check
+
+r_v_c ((V c),(V v,s):[],p)
+	|c==v = [(V v,p)]
+	|otherwise = []
+r_v_c ((V c),(V v, s):vs,p)
+	|c==v = [(V v,p)]
+	|otherwise = r_v_c((V c),vs,p)
+
+--rename
+
+r_v_r (c,v) = r_v_r_r (c,v,v)
+
+r_v_r_r ([],v,x)= []
+r_v_r_r ((T (Identity (Var (V c)))):cs,[],x) = (T (Identity (Var (V c)))):(r_v_r(cs,x))
+r_v_r_r ((T (Identity (Var (V c)))):cs,(V v,s):vs,x)
+	|c==v 		= (T (Identity (Var (V s)))):(r_v_r(cs,x))
+	|otherwise 	= (r_v_r_r((T (Identity (Var (V c)))):cs,vs,x))
+
+
+--remove quantors
+
+remove_quantor (F (Identity (Quant All cont op1)))		= remove_quantor (op1)
+remove_quantor (F (Identity (Quant Exists cont op1)))	= remove_quantor (op1)
+remove_quantor (F (Identity ((:~:) op1)))				= (F (Identity ((:~:) (remove_quantor(op1)))))
+remove_quantor (F (Identity (BinOp op1 op op2))) 		= (F (Identity (BinOp (remove_quantor(op1)) op (remove_quantor(op2)))))
+remove_quantor f = f
+
+--add quantors to beginning
+
+add_quantor (formula,(x,y)) = (F (Identity (Quant All x (F (Identity (Quant Exists y (formula)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -97,14 +227,14 @@ walk_f (F (Identity (BinOp op1 (:|:)   op2))) 	= (F (Identity (BinOp (walk_f(op1
 
 scf_formula (f,path)
 	|path == []			= f
-	|last path =="A" 	= scf_top(scf_trans_a_X(f,path))
+	|last path =='A' 	= scf_top(scf_trans_a_X(f,path))
 	|otherwise 			= f
 
 scf_path (f,path) = path
 
 scf_merge ((f1,path1),(f2,path2),(F (Identity (BinOp op1 (:|:) op2))),path)
-	|last path1 =="A"	= ((F (Identity (BinOp f1  (:|:) op2))),path1)
-	|last path2 =="A"	= ((F (Identity (BinOp op1 (:|:) f2 ))),path2)
+	|last path1 =='A'	= ((F (Identity (BinOp f1  (:|:) op2))),path1)
+	|last path2 =='A'	= ((F (Identity (BinOp op1 (:|:) f2 ))),path2)
 	|otherwise 			= ((F (Identity (BinOp op1 (:|:) op2))),path)
 
 scf_negate x = (F (Identity ((:~:) x)))
@@ -117,6 +247,8 @@ scf_top (F (Identity (BinOp op1 (:&:) op2)))= (F (Identity (BinOp (scf_top(op1))
 scf_top (F (Identity (PredApp aw t))) = (F (Identity (PredApp aw t)))
 scf_top (F (Identity ((:~:) (F (Identity (PredApp aw t)))))) = (F (Identity ((:~:) (F (Identity (PredApp aw t))))))
 scf_top (F (Identity( (:~:) (F (Identity( ((:~:) op1))))))) = scf_top(op1)
+scf_top (F (Identity (Quant q cont op1))) = (F (Identity (Quant q cont (scf_top(op1)))))
+
 scf_top x = scf_formula(scf_trans_b (x,[]))
 
 --transformation level (disjunctions)
@@ -126,8 +258,8 @@ scf_trans_b ((F (Identity (PredApp aw t))),path) 							= ((F (Identity (PredApp
 scf_trans_b ((F (Identity ((:~:) (F (Identity (PredApp aw t)))))),path) 	= ((F (Identity ((:~:) (F (Identity (PredApp aw t)))))) ,path)
 
 --beta formulas
-scf_trans_b ((F (Identity (BinOp op1 (:|:) op2))) 							,path) = scf_merge(scf_trans_b(op1,path++["L"]),scf_trans_b(op2,path++["R"]),(F (Identity (BinOp op1 (:|:) op2))),path)
-scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2)))))) 	,path) = scf_merge(scf_trans_b(op1,path++["L"]),scf_trans_b(op2,path++["R"]),(F (Identity (BinOp op1 (:|:) op2))),path)
+scf_trans_b ((F (Identity (BinOp op1 (:|:) op2))) 							,path) = scf_merge(scf_trans_b(op1,path++['L']),scf_trans_b(op2,path++['R']),(F (Identity (BinOp op1 (:|:) op2))),path)
+scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2)))))) 	,path) = scf_merge(scf_trans_b(op1,path++['L']),scf_trans_b(op2,path++['R']),(F (Identity (BinOp op1 (:|:) op2))),path)
 scf_trans_b ((F (Identity( (:~:) (F (Identity( ((:~:) op1))))))) 			,path) = scf_trans_b (op1,path)
 scf_trans_b ((F (Identity (BinOp op1 (:=>:) op2))) 							,path) = scf_trans_b ((F (Identity (BinOp (scf_negate(op1)) (:|:) op2))) 				,path)
 scf_trans_b ((F (Identity (BinOp op1 (:<=:) op2))) 							,path) = scf_trans_b ((F (Identity (BinOp op1 			  (:|:) (scf_negate(op2))))),path)
@@ -135,35 +267,46 @@ scf_trans_b ((F (Identity (BinOp op1 (:~&:)  op2)))							,path) = scf_trans_b (
 scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,path) = scf_trans_b ((F (Identity (BinOp (scf_negate(op1)) (:|:) (scf_negate(op2))))),path)
 
 --alpha formulas
-scf_trans_b ((F (Identity (BinOp op1 (:&:) op2)))							,path) = ((F (Identity (BinOp op1 (:&:) op2)))							,path++["A"])
-scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2)))))) 	,path++["A"])
-scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))) ,path++["A"])
-scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))) ,path++["A"])
-scf_trans_b ((F (Identity (BinOp op1 (:~|:)  op2)))							,path) = ((F (Identity (BinOp op1 (:~|:)  op2)))						,path++["A"])
-scf_trans_b ((F (Identity (Quant q cont op1)))								,path) = ((F (Identity (Quant q cont (scf_formula(scf_trans_b(op1,path)))))),scf_path(scf_trans_b(op1,path)))
+scf_trans_b ((F (Identity (BinOp op1 (:&:) op2)))							,path) = ((F (Identity (BinOp op1 (:&:) op2)))							,path++"A")
+scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2)))))) 	,path++"A")
+scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))) ,path++"A")
+scf_trans_b ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2))))))	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))) ,path++"A")
+scf_trans_b ((F (Identity (BinOp op1 (:~|:)  op2)))							,path) = ((F (Identity (BinOp op1 (:~|:)  op2)))						,path++"A")
+scf_trans_b ((F (Identity (Quant q cont op1)))								,path) = ((F (Identity (Quant q cont (scf_top(op1))))),path)
 
 
 scf_trans_a_X (f,path) = (F (Identity (BinOp (scf_trans_a_L(f,path)) (:&:) (scf_trans_a_R(f,path)))))
 
 scf_trans_a_L (f,[])=f
-scf_trans_a_L ((F (Identity (BinOp op1 (:&:) op2)))							,["A"]) = op1
-scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,["A"]) = scf_negate(op1)
-scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))),["A"]) = op1
-scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))),["A"]) = scf_negate(op1)
-scf_trans_a_L ((F (Identity (BinOp op1 (:~|:)  op2)))						,["A"]) = scf_negate(op1)
+scf_trans_a_L ((F (Identity (BinOp op1 (:&:) op2)))							,"A") = op1
+scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,"A") = scf_negate(op1)
+scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))),"A") = op1
+scf_trans_a_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))),"A") = scf_negate(op1)
+scf_trans_a_L ((F (Identity (BinOp op1 (:~|:)  op2)))						,"A") = scf_negate(op1)
 scf_trans_a_L ((F (Identity (BinOp op1 op op2)))							,x:xs)
-	|x=="L" = (F (Identity (BinOp (scf_trans_a_L(op1,xs)) op op2)))
-	|x=="R" = (F (Identity (BinOp op1 op (scf_trans_a_L(op2,xs)))))
+	|x=='L' = (F (Identity (BinOp (scf_trans_a_L(op1,xs)) op op2)))
+	|x=='R' = (F (Identity (BinOp op1 op (scf_trans_a_L(op2,xs)))))
 
 scf_trans_a_R (f,[])=f
-scf_trans_a_R ((F (Identity (BinOp op1 (:&:) op2)))						 	,["A"]) = op2
-scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,["A"]) = scf_negate(op2)
-scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))),["A"]) = scf_negate(op2)
-scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))),["A"]) = op2
-scf_trans_a_R ((F (Identity (BinOp op1 (:~|:)  op2)))						,["A"]) = scf_negate(op2)
+scf_trans_a_R ((F (Identity (BinOp op1 (:&:) op2)))						 	,"A") = op2
+scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,"A") = scf_negate(op2)
+scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2)))))),"A") = scf_negate(op2)
+scf_trans_a_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2)))))),"A") = op2
+scf_trans_a_R ((F (Identity (BinOp op1 (:~|:)  op2)))						,"A") = scf_negate(op2)
 scf_trans_a_R ((F (Identity (BinOp op1 op op2)))							,x:xs)
-	|x=="L" = (F (Identity (BinOp (scf_trans_a_R(op1,xs)) op op2)))		
-	|x=="R" = (F (Identity (BinOp op1 op (scf_trans_a_R(op2,xs)))))
+	|x=='L' = (F (Identity (BinOp (scf_trans_a_R(op1,xs)) op op2)))		
+	|x=='R' = (F (Identity (BinOp op1 op (scf_trans_a_R(op2,xs)))))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -176,14 +319,14 @@ scf_trans_a_R ((F (Identity (BinOp op1 op op2)))							,x:xs)
 
 dcf_formula (f,path)
 	|path == []			= f
-	|last path =="B" 	= dcf_top(dcf_trans_b_X(f,path))
+	|last path =='B' 	= dcf_top(dcf_trans_b_X(f,path))
 	|otherwise 			= f
 
 dcf_path (f,path) = path
 
 dcf_merge ((f1,path1),(f2,path2),(F (Identity (BinOp op1 (:|:) op2))),path)
-	|last path1 =="B"	= ((F (Identity (BinOp f1  (:|:) op2))),path1)
-	|last path2 =="B"	= ((F (Identity (BinOp op1 (:|:) f2 ))),path2)
+	|last path1 =='B'	= ((F (Identity (BinOp f1  (:|:) op2))),path1)
+	|last path2 =='B'	= ((F (Identity (BinOp op1 (:|:) f2 ))),path2)
 	|otherwise 			= ((F (Identity (BinOp op1 (:|:) op2))),path)
 
 dcf_negate x = (F (Identity ((:~:) x)))
@@ -196,6 +339,8 @@ dcf_top (F (Identity (BinOp op1 (:|:) op2)))= (F (Identity (BinOp (dcf_top(op1))
 dcf_top (F (Identity (PredApp aw t))) = (F (Identity (PredApp aw t)))
 dcf_top (F (Identity ((:~:) (F (Identity (PredApp aw t)))))) = (F (Identity ((:~:) (F (Identity (PredApp aw t))))))
 dcf_top (F (Identity( (:~:) (F (Identity( ((:~:) op1))))))) = dcf_top(op1)
+dcf_top (F (Identity (Quant q cont op1))) = (F (Identity (Quant q cont (dcf_top(op1)))))
+
 dcf_top x = dcf_formula(dcf_trans_a (x,[]))
 
 --transformation level (conjunctions)
@@ -205,12 +350,12 @@ dcf_trans_a ((F (Identity (PredApp aw t))),path) 							= ((F (Identity (PredApp
 dcf_trans_a ((F (Identity ((:~:) (F (Identity (PredApp aw t)))))),path) 	= ((F (Identity ((:~:) (F (Identity (PredApp aw t)))))) ,path)
 
 --beta formulas
-dcf_trans_a ((F (Identity (BinOp op1 (:|:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:|:)  op2))) 		,path++["B"])
-dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2)))))) 	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 	(:~|:) op2))))))	,path++["B"])
-dcf_trans_a ((F (Identity (BinOp op1 (:=>:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:=>:) op2))) 		,path++["B"])
-dcf_trans_a ((F (Identity (BinOp op1 (:<=:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:<=:) op2))) 		,path++["B"])
-dcf_trans_a ((F (Identity (BinOp op1 (:~&:)  op2)))							,path) = ((F (Identity (BinOp op1 						(:~&:) op2)))		,path++["B"])
-dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 	(:&:)  op2)))))) 	,path++["B"])
+dcf_trans_a ((F (Identity (BinOp op1 (:|:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:|:)  op2))) 		,path++"B")
+dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2)))))) 	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 	(:~|:) op2))))))	,path++"B")
+dcf_trans_a ((F (Identity (BinOp op1 (:=>:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:=>:) op2))) 		,path++"B")
+dcf_trans_a ((F (Identity (BinOp op1 (:<=:) op2))) 							,path) = ((F (Identity (BinOp op1 						(:<=:) op2))) 		,path++"B")
+dcf_trans_a ((F (Identity (BinOp op1 (:~&:)  op2)))							,path) = ((F (Identity (BinOp op1 						(:~&:) op2)))		,path++"B")
+dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,path) = ((F (Identity ((:~:) (F (Identity (BinOp op1 	(:&:)  op2)))))) 	,path++"B")
 
 --alpha formulas
 dcf_trans_a ((F (Identity (BinOp op1 (:&:) op2)))							,path) = dcf_trans_a((F (Identity (BinOp op1 		(:|:)  op2)))				,path)
@@ -218,29 +363,29 @@ dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:|:) op2))))))	,path) 
 dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:=>:)  op2))))))	,path) = dcf_trans_a((F (Identity (BinOp op1 				(:=>:) (dcf_negate(op2)))))	,path)
 dcf_trans_a ((F (Identity ((:~:) (F (Identity (BinOp op1 (:<=:)  op2))))))	,path) = dcf_trans_a((F (Identity (BinOp (dcf_negate(op1)) (:<=:) op2))) 				,path)
 dcf_trans_a ((F (Identity (BinOp op1 (:~|:)  op2)))							,path) = dcf_trans_a((F (Identity (BinOp op1 				(:|:)  op2)))				,path)
-dcf_trans_a ((F (Identity (Quant q cont op1)))								,path) = ((F (Identity (Quant q cont (dcf_formula(dcf_trans_a(op1,path)))))),dcf_path(dcf_trans_a(op1,path)))
+dcf_trans_a ((F (Identity (Quant q cont op1)))								,path) = ((F (Identity (Quant q cont (dcf_top(op1))))),path)
 
 dcf_trans_b_X (f,path) = (F (Identity (BinOp (dcf_trans_b_L(f,path)) (:|:) (dcf_trans_b_R(f,path)))))
 
 dcf_trans_b_L (f,[])=f
-dcf_trans_b_L ((F (Identity (BinOp op1 (:|:) op2))) 						,["B"]) = op1
-dcf_trans_b_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2))))))	,["B"]) = op1
-dcf_trans_b_L ((F (Identity (BinOp op1 (:=>:) op2))) 						,["B"]) = dcf_negate(op1)
-dcf_trans_b_L ((F (Identity (BinOp op1 (:<=:) op2))) 						,["B"]) = op1
-dcf_trans_b_L ((F (Identity (BinOp op1 (:~&:)  op2)))						,["B"]) = dcf_negate(op1)
-dcf_trans_b_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,["B"]) = dcf_negate(op1)
+dcf_trans_b_L ((F (Identity (BinOp op1 (:|:) op2))) 						,"B") = op1
+dcf_trans_b_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2))))))	,"B") = op1
+dcf_trans_b_L ((F (Identity (BinOp op1 (:=>:) op2))) 						,"B") = dcf_negate(op1)
+dcf_trans_b_L ((F (Identity (BinOp op1 (:<=:) op2))) 						,"B") = op1
+dcf_trans_b_L ((F (Identity (BinOp op1 (:~&:)  op2)))						,"B") = dcf_negate(op1)
+dcf_trans_b_L ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,"B") = dcf_negate(op1)
 dcf_trans_b_L ((F (Identity (BinOp op1 op op2)))							,x:xs)
-	|x=="L" = (F (Identity (BinOp (dcf_trans_b_L(op1,xs)) op op2)))
-	|x=="R" = (F (Identity (BinOp op1 op (dcf_trans_b_L(op2,xs)))))
+	|x=='L' = (F (Identity (BinOp (dcf_trans_b_L(op1,xs)) op op2)))
+	|x=='R' = (F (Identity (BinOp op1 op (dcf_trans_b_L(op2,xs)))))
 
 
 dcf_trans_b_R (f,[])=f
-dcf_trans_b_R ((F (Identity (BinOp op1 (:|:) op2))) 						,["B"]) = op2
-dcf_trans_b_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2))))))	,["B"]) = op2
-dcf_trans_b_R ((F (Identity (BinOp op1 (:=>:) op2))) 						,["B"]) = op2
-dcf_trans_b_R ((F (Identity (BinOp op1 (:<=:) op2))) 						,["B"]) = dcf_negate(op2)
-dcf_trans_b_R ((F (Identity (BinOp op1 (:~&:)  op2)))						,["B"]) = dcf_negate(op2)
-dcf_trans_b_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,["B"]) = dcf_negate(op2)
+dcf_trans_b_R ((F (Identity (BinOp op1 (:|:) op2))) 						,"B") = op2
+dcf_trans_b_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:~|:) op2))))))	,"B") = op2
+dcf_trans_b_R ((F (Identity (BinOp op1 (:=>:) op2))) 						,"B") = op2
+dcf_trans_b_R ((F (Identity (BinOp op1 (:<=:) op2))) 						,"B") = dcf_negate(op2)
+dcf_trans_b_R ((F (Identity (BinOp op1 (:~&:)  op2)))						,"B") = dcf_negate(op2)
+dcf_trans_b_R ((F (Identity ((:~:) (F (Identity (BinOp op1 (:&:) op2)))))) 	,"B") = dcf_negate(op2)
 dcf_trans_b_R ((F (Identity (BinOp op1 op op2)))							,x:xs)
-	|x=="L" = (F (Identity (BinOp (dcf_trans_b_R(op1,xs)) op op2)))
-	|x=="R" = (F (Identity (BinOp op1 op (dcf_trans_b_R(op2,xs)))))
+	|x=='L' = (F (Identity (BinOp (dcf_trans_b_R(op1,xs)) op op2)))
+	|x=='R' = (F (Identity (BinOp op1 op (dcf_trans_b_R(op2,xs)))))
