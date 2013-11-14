d4bbc37f74d452190d1126f994dfd2dbf15ec42b
zentol <s.motsu@web.de>
2013-11-14 12:35:23 +0100

    renamed functions, added walk
    renamed t2-t6 to be more comprehensible
    walk runs through a formula, basically checking if all patterns are
    implemented (quant is missing atm)

--- Codec/TPTP.hs
+++ Codec/TPTP.hs
@@ -19,22 +19,53 @@ t_a = parse "fof(test,axiom,~(a&b))."
 t_b = parse "fof(test,axiom,(~a&b))."
 t_negate =  parse "fof(test,axiom,a)."
 t_negate2 = parse "fof(test,axiom,~a)."
+t_eq = parse "fof(test,axiom,a<=>b)."
+t_quant = parse "fof(test,axiom,? [A,Y] :fix(A))."
+t_pred = parse "fof(test,axiom,fix(A,B))."
 
 --Listof AFOrmula
-t2 (x:xs)= x
+get_list_element (x:xs)= x
 
-t3 (AFormula a b c d) = c
+--always used first
+get_formula (AFormula a b c d) = c
 
-t4 (F x) = x
+enter_formula (F x) = x
 
-t5 (Identity x) = x
+enter_identity (Identity x) = x
 
-t6 (BinOp a x b) = x
+enter_binop (BinOp a x b) = x
 
-tt (AFormula a b (F (Identity(BinOp r x s))) d) = x
+-- <=> | <= | => | <~> | ~| | ~& | "|" | &
+--(F (Identity (BinOp op1 op op2)))
 
-tb_negate_term x = x
+walk_t [] = []
+walk_t ((T (Identity( Var v))):[]) = ((T (Identity( Var v))):[])
+walk_t ((T (Identity( Var v))):xs) = ((T (Identity( Var v))):walk_t(xs))
 
+walk_f (F (Identity( PredApp aw t)))			= (F (Identity( PredApp aw (walk_t(t)))))
+walk_f (F (Identity( (:~:) op1)))				= (F (Identity( (:~:) (walk_f(op1)))))
+walk_f (F (Identity( BinOp op1 (:<=>:) op2))) = (F (Identity( BinOp (walk_f(op1)) (:<=>:) (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:<=:)  op2))) = (F (Identity( BinOp (walk_f(op1)) (:<=:)  (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:=>:)  op2))) = (F (Identity( BinOp (walk_f(op1)) (:=>:)  (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:<~>:) op2))) = (F (Identity( BinOp (walk_f(op1)) (:<~>:) (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:~|:)  op2))) = (F (Identity( BinOp (walk_f(op1)) (:~|:)  (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:~&:)  op2))) = (F (Identity( BinOp (walk_f(op1)) (:~&:)  (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:&:)   op2))) = (F (Identity( BinOp (walk_f(op1)) (:&:)   (walk_f(op2)))))
+walk_f (F (Identity( BinOp op1 (:|:)   op2))) = (F (Identity( BinOp (walk_f(op1)) (:|:)   (walk_f(op2)))))
 
-tb_negate_binop (BinOp op1 (:&:) op2) = (BinOp (tb_negate_term(op1)) (:|:) (tb_negate_term(op2)))
-tb_negate_binop (BinOp op1 (:|:) op2) = (BinOp (tb_negate_term(op1)) (:&:) (tb_negate_term(op2)))
+-- ~
+--(F (Identity ((:~:) op1))
+
+-- atomicword
+--(F (Identity (PredApp op1)))
+
+-- ! | ?
+--type = All | Exists
+--cont = [V "A",...]
+--(F (Identity (Quant type cont op1)))
+
+-- part of predicate
+--(T (Identity (Var (V x))))
+
+tb_negate_binop (BinOp op1 (:&:) op2) = (BinOp op1 (:|:) op2)
+tb_negate_binop (BinOp op1 (:|:) op2) = (BinOp op1 (:&:) op2)
